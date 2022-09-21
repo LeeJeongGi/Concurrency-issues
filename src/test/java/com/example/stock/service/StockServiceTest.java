3f22096bacdc4e683bb317b6709047e4483bbf5c
LeeJeongGi <apple4rhk@naver.com>
2022-09-21 22:35:35 +0900

    test : laze condition 이 발생하는 환경 만들기

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -39,6 +43,30 @@ class StockServiceTest {
         Stock stock = stockRepository.findById(1L).orElseThrow();
 
         assertEquals(99, stock.getQuantity());
+    }
+
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+
+        // 비동기로 실행하는 작업을 단순화 하여 사용할수 있게 해주는 자바의 API
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        // 100개의 작업이 끝날때까지 기달려야 하므로 사용한다.
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                stockService.decrease(1L, 1L);
+            });
+        }
+
+        // 다른 쓰레드 작업이 끝날 때까지 대기할 수 있게 도와주는 클래스
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // 100 - (1 * 100) = 0 -> 예상 작업
+        assertEquals(0L, stock.getQuantity());
     }
 
 }
