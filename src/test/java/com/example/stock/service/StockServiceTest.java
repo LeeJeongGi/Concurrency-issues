2c6c6fcaf70c05f7c6304d194db9b64ab0c8da3f
LeeJeongGi <apple4rhk@naver.com>
2022-09-22 22:43:39 +0900

    test : PessimisticLock 을 통해 동시성 문제 해결 테스트

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class StockServiceTest {
 
     @Autowired
-    private StockService stockService;
+    private PessimisticLockStockService stockService;
 
     @Autowired
     private StockRepository stockRepository;
