0eb15f61f6f3f1fd3fc15b825fb887291417d0a1
LeeJeongGi <apple4rhk@naver.com>
2022-09-23 17:54:41 +0900

    test : NamedLock 을 통해 동시성 문제 해결 테스트

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -3,6 +3,7 @@ package com.example.stock.service;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -14,7 +15,7 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-//    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public synchronized void decrease(Long id, Long quantity) {
         Stock stock = stockRepository.findById(id).orElseThrow();
 
